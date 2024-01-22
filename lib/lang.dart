7df225dd44d4e7a5b88bd0bb5706087d08d68107
Victor Jimenez <43785784+victor7w7r@users.noreply.github.com>
2024-01-21 19:45:14 -0500

    RemoveAnnotations

--- lib/lang.dart
+++ lib/lang.dart
@@ -17,15 +17,15 @@ void initLang() {
     'pacstrap no está disponible, instalando',
     'Todo ok!',
     'No existen dispositivos de disco disponibles en tu sistema, saliendo',
-    'Número: ', //12
+    'Número: ',
     'Seleccione su dispositivo de destino para la instalación\n',
     'Este dispositivo tiene una tabla de tipo DOS o no está inicializado '
         '¿Quieres inicializarlo con GPT?\n',
     '¡ATENCION!: Tu dispositivo de destino será formateado y vaciado\n',
     'Inicializando / Creando partición EFI\n\n',
-    'Presione Enter para continuar...', //17
+    'Presione Enter para continuar...',
     'Este dispositivo está usando con otras particiones pero no tiene partición EFI, '
-        '¿Quieres crear una partición EFI?\n', //18
+        '¿Quieres crear una partición EFI?\n',
     'Existe una partición EFI pero no es está en la posición *1, el dispositivo se tiene que borrar entero, '
         '¿Quieres continuar?\n',
     'No existe ninguna partición ext4 o compatible con Linux para instalar, '
@@ -40,22 +40,22 @@ void initLang() {
     '=============== PACSTRAP: INSTALAR LA BASE DE LINUX Y PAQUETES CORE  =============== \n',
     'Algo falló en el chroot, no se desmontarán los sistemas de archivos, '
         'así que puedes investigar.',
-    '=============== CONTRASEÑA DE ROOT PARA EL SISTEMA =============== \n', //30
+    '=============== CONTRASEÑA DE ROOT PARA EL SISTEMA =============== \n',
     'Selecciona tu procesador \n',
     '=============== CONFIGURAR GRUB =============== \n',
-    '=============== INICIAR NETWORKMANAGER Y SERVICIOS DE SSH =============== \n', //33
+    '=============== INICIAR NETWORKMANAGER Y SERVICIOS DE SSH =============== \n',
     'Por favor escriba su hostname (ej: A036-arch): ',
     'America/Guayaquil es el timezone por defecto, si quieres cambiarlo por '
         'algun otro, aquí está la orden \n'
         'ln -sf /usr/share/zoneinfo/REGION/CITY /etc/localtime',
     'Elige tu Locale, si quieres cambiar a otros, revisa el README '
         'dentro del GitHub de este proyecto\n',
-    '=============== AGREGAR UN USUARIO DE SUDO =============== \n', //37
-    'Escribe tu nuevo usuario: ', //38
+    '=============== AGREGAR UN USUARIO DE SUDO =============== \n',
+    'Escribe tu nuevo usuario: ',
     'Selecciona un GUI, estos son los mas usados, Este script recomienda XFCE\n',
-    'Elige tu controlador de GPU\n', //40
-    '=============== AUR (CHAOTIC-AUR / LIQUORIX KERNEL) ===============  \n', //41
-    '¿Te gusta BlackArch? (Repositorio de Penetration Testing para Arch Linux)\n', //42
+    'Elige tu controlador de GPU\n',
+    '=============== AUR (CHAOTIC-AUR / LIQUORIX KERNEL) ===============  \n',
+    '¿Te gusta BlackArch? (Repositorio de Penetration Testing para Arch Linux)\n',
     'Has instalado xfce antes, ¿te gustaría instalar mi template?\n',
     '=============== OPTIMIZACIONES =============== \n',
     'LISTO!!!, Tu PC ya instalo de manera correcta a Arch Linux, '
@@ -76,15 +76,15 @@ void initLang() {
     'pacstrap is not available in this system, installing',
     'All dependencies is ok!',
     'There are not available disk devices in your system, exiting',
-    'Number: ', //12
+    'Number: ',
     'Choose your destination device for install\n',
     'This device has a DOS Label Type or is not initialized '
         'Do you want to initialize it with GPT?\n',
     'WARNING!: Your destination device would be formatted and empty\n',
     'Initializing / Creating EFI partition\n\n',
-    'Press Enter to continue...', //17
+    'Press Enter to continue...',
     'This device is using with other partitions but not have EFI partition, '
-        'Do you want to create a EFI partition?\n', //18
+        'Do you want to create a EFI partition?\n',
     'There is a EFI partition but is not in the *1 position, '
         'the device must be erased, Do you want to continue?\n',
     'There is not any ext4 or compatible partition to install, '
@@ -100,21 +100,21 @@ void initLang() {
     '=============== FORMAT ROOT FILESYSTEM AND MOUNTING =============== \n',
     '=============== PACSTRAP: INSTALL LINUX BASE AND CORE PACKAGES =============== \n',
     'Something failed inside the chroot, not unmounting filesystems so you can investigate.',
-    '=============== ROOT PASSWORD FOR YOUR SYSTEM =============== \n', //30
+    '=============== ROOT PASSWORD FOR YOUR SYSTEM =============== \n',
     'Choose your processor \n',
-    '=============== CONFIGURE GRUB =============== \n', //32
-    '=============== START NETWORKMANAGER AND SSH SERVICES =============== \n', //33
+    '=============== CONFIGURE GRUB =============== \n',
+    '=============== START NETWORKMANAGER AND SSH SERVICES =============== \n',
     'Please write your hostname (ex: A036-arch): ',
     'America/Guayaquil is the timezone by default, if you want to change, '
         'here is the command \n ln -sf /usr/share/zoneinfo/REGION/CITY /etc/localtime',
     'Choose your locale, if you want to change to other locales, '
-        'check the README of the Github of this project\n', //36
+        'check the README of the Github of this project\n',
     '=============== ADD A USER TO A SUDO GROUP =============== \n',
-    'Write your new user: ', //38
+    'Write your new user: ',
     'Choose a GUI, these are the common used, this script recommends XFCE\n',
     'Choose your GPU drivers\n',
-    '=============== AUR (CHAOTIC-AUR / LIQUORIX KERNEL) ===============  \n', //41
-    'Do you like BlackArch? (Penetration Testing Repository for Arch Linux)\n', //42
+    '=============== AUR (CHAOTIC-AUR / LIQUORIX KERNEL) ===============  \n',
+    'Do you like BlackArch? (Penetration Testing Repository for Arch Linux)\n',
     'You have installed xfce before, do you want to install my template?\n',
     '=============== OPTIMIZATIONS =============== \n',
     'READY!!!, Your PC is succesfully installed with Arch Linux, '
